6b1549e28ffa7b7eea115afb7837ee3c3e5e17bc
devanshgl66 <devanshgl55@gmail.com>
2020-07-07 00:16:24 +0530

    minor changes

--- cors.js
+++ cors.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const whitelist = ['http://localhost:3000', 'https://localhost:3443','https://bluegrassrestaurant.herokuapp.com/'];
+const whitelist = ['http://localhost:3000', 'https://localhost:3443','https://bluegrassrestaurant.herokuapp.com'];
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     console.log(req.header('Origin'));
